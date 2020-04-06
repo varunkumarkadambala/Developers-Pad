0bf7415ed4c76f624517f9a5865a4fe69e8751d9
Varun <varunkmr26@gmail.com>
2020-04-06 22:41:18 +0530

    Added User Model

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req,res) => res.send("API is Running"));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/postsgit '));
+app.use('/api/posts', require('./routes/api/posts'));
 
 PORT = process.env.PORT || 5000;
 
