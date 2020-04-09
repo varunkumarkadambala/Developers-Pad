6791e1b7981549a69fa014a50a9713f3b81d3a8a
Varun <varunkmr26@gmail.com>
2020-04-09 20:04:59 +0530

    Create react app and landing page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React,{ Fragment} from 'react';
+
+//Import Components
+import Navbar from "./components/skeleton/Navbar";
+import Landing from "./components/skeleton/Landing";
+import './App.css';
+
+const App = () => (
+    <Fragment>
+        <Navbar/>
+        <Landing/>
+    </Fragment>
+);
+
+export default App;
