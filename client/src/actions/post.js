54d688a881fa906a5fdfc770084e52c2aa574bd1
varunkumarkadambala <varunkmr26@gmail.com>
2020-06-04 12:34:59 +0530

    Add a new post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_POSTS, POST_ERROR, UPDATE_LIKES,
-        DELETE_POST} from './constants';
+        DELETE_POST, ADD_POST} from './constants';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -65,6 +65,31 @@ export const deletePost = id => async dispatch => {
         });
 
         dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post('/api/posts', formData, config);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,5 +1,5 @@
 import { GET_POSTS, POST_ERROR, UPDATE_LIKES,
-        DELETE_POST} from '../actions/constants';
+        DELETE_POST, ADD_POST} from '../actions/constants';
 
 const initialState = {
     posts: [],
@@ -16,6 +16,12 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            };
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [payload, ...state.posts],
                 loading: false
             };
         case DELETE_POST:
